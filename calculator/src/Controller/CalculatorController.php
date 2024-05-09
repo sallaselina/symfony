c070b6e1e83821a588d5286265014619db8c1fd9
Salla Närhi <salla.narhi97@gmail.com>
2024-05-09 14:11:32 +0300

    add starshop/cosmic coding with symfonycasts exercise

--- calculator/src/Controller/CalculatorController.php
+++ calculator/src/Controller/CalculatorController.php
@@ -13,27 +13,34 @@ class CalculatorController extends AbstractController {
     $number1 = $request->query->get("number1", "");
     $number2 = $request->query->get("number2", "");
     $calculations = [];
-
-        if (!empty($number1) && !empty($number2)) {
+    if ($number1 && $number2) {
+  /*   if ($number2 == 0) {
+        echo "Cannot divide by zero."; */
+        if (is_numeric($number1) && is_numeric($number2) && $number2 != 0) {
             $calculations = [
-                "addition " . $number1 . "+" . $number2  => $number1 + $number2,
-                "subtraction ". $number1 . "-" . $number2 => $number1 - $number2,
-                "multiplication ". $number1 . "*" . $number2 => $number1 * $number2,
-                "division ". $number1 . "/" . $number2 => $number1 / $number2,
-               ];
-        }
-        else if (!is_numeric($number1) || !is_numeric($number2)) {
-            echo "Enter two numbers.";
-        }
-        else {
+            "addition " . $number1 . "+" . $number2  => $number1 + $number2,
+            "subtraction ". $number1 . "-" . $number2 => $number1 - $number2,
+            "multiplication ". $number1 . "*" . $number2 => $number1 * $number2,
+            "division ". $number1 . "/" . $number2 => $number1 / $number2,
+       ];
+    } } 
+    else  {
+        echo "Please enter two numbers.";
+    }
+    return $this->render("calculator/index.html.twig",[ 
+        "calculations" => $calculations,
+        "number1" => $number1,
+        "number2" => $number2]
+    );
+}
+}
+        
+    
+        /* else {
             echo "Fields cannot be empty.";
-        }
-        return $this->render("calculator/index.html.twig",[ 
-    "calculations" => $calculations,
-    "number1" => $number1,
-    "number2" => $number2]
-);
-    }}
+        } */
+
+    
          
     
        
